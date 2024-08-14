c41e7e7df373f9e4cdd0f618b721f8dbf658de01
Rachit Srivastava <honey032004@gmail.com>
2024-08-14 18:10:34 +0530

    OOPS

---
+++ OOPS/classandobjects.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+using namespace std;
+
+ class Employee{
+
+public:
+string Name;
+string Company;
+int age;
+
+};
+
+int main(){
+
+
+Employee employee1;
+employee1.Name = "Rachit";
+employee1.Company = "Microsoft";
+employee1.age = 21;
+cout << employee1.Company << endl;
+
+}
