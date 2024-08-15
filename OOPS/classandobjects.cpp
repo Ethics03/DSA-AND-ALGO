215bbbacdd32ebd14e029dea2ed5d1990a338c2d
Rachit Srivastava <honey032004@gmail.com>
2024-08-15 17:31:07 +0530

    constructors and setget

--- OOPS/classandobjects.cpp
+++ OOPS/classandobjects.cpp
@@ -4,21 +4,56 @@
 using namespace std;
 
  class Employee{
-
+private:
+    string Name;
+    string Company;
+    int Age;
 public:
-string Name;
-string Company;
-int age;
+    //setter
+    void setname(string name){
+        Name = name;
+    }
+    //getter
+    string getName(){
+        return Name;
+    }
+    void setcompany(string company){
+        Company = company;
+    }
+    //getter
+    string getCompany(){
+        return Company;
+    }
+    void setAge(int age){
+        Age = age;
+    }
+    //getter
+    int getAge(){
+        return Age;
+    }
+    void intro(){
+        cout << "Name : " << Name + " : "<< "Company: "<< Company + " : "<< " Age : " << Age << endl;
+
+}
+//USING CONSTRUCTOR FOR DEFAULT VALUES or as a parameter
+    Employee(string name , string company , int age){
+    Name = name;
+    Company = company;
+    Age = age;
+}
+
 
 };
 
 int main(){
 
+Employee employee1 = Employee(" Rachit "," Microsoft ",21);
 
-Employee employee1;
-employee1.Name = "Rachit";
-employee1.Company = "Microsoft";
-employee1.age = 21;
-cout << employee1.Company << endl;
+employee1.intro();
+
+employee1.setAge(24);
+
+cout << employee1.getName() << " is " << employee1.getAge() << " years old" << endl;
+
 
 }
